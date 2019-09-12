44c81a3214141199336ec934bd1cfa6a86781949
Cecile <ce.rob.gat@gmail.com>
2019-09-12 11:10:41 +0200

    fonc price

---
+++ src/MonteCarlo.cpp
@@ -0,0 +1,24 @@
+//
+// Created by cecile on 12/09/19.
+//
+#include <iostream>
+#include "MonteCarlo.hpp"
+
+void price(double &prix, double &ic){
+
+    //Initialisation des variables necessaires
+    double sum = 0;
+    PnlMat *path = pnl_mat_create();
+    int nbAssets = this.mod_->size_;
+    int nbTimeSteps = this.opt_->nbTimeSteps_;
+
+    //Calcul du prix a t=0
+    for(int i = 0; i < this.nbSamples_; i++){
+
+    }
+
+    //Calcul de l'estimateur
+
+    //Calcul de l'intervalle de confiance a 95%
+}
+
