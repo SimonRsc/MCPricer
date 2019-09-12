ae4f596d06c09ae4a9985ecde2830aa73233bb26
Cecile <ce.rob.gat@gmail.com>
2019-09-12 11:51:52 +0200

    fonc price montecarlo

--- src/MonteCarlo.cpp
+++ src/MonteCarlo.cpp
@@ -4,21 +4,39 @@
 #include <iostream>
 #include "MonteCarlo.hpp"
 
-void price(double &prix, double &ic){
+using namespace std;
+
+void MonteCarlo::price(double &prix, double &ic){
 
     //Initialisation des variables necessaires
     double sum = 0;
-    PnlMat *path = pnl_mat_create();
-    int nbAssets = this.mod_->size_;
-    int nbTimeSteps = this.opt_->nbTimeSteps_;
+    double sumSquare = 0;
+    double phi =0;
+    int nbAssets = this->mod_->size_;
+    int nbTimeSteps = this->opt_->nbTimeSteps_;
+    PnlMat *path = pnl_mat_create(nbTimeSteps+1, nbAssets);
+
+    double estim = 0;
+
 
     //Calcul du prix a t=0
-    for(int i = 0; i < this.nbSamples_; i++){
-
+    for(int i = 0; i < this->nbSamples_; i++){
+        mod_->asset(path, this->opt_->T_, nbTimeSteps, this->rng_);
+        phi = this->opt_->payoff(path);
+        sum += phi;
+        sumSquare += pow(phi, 2);
     }
+
+
+
+    prix = exp(-this->mod_->r_ * this->opt_->T_) * sum / this->nbSamples_;
 
     //Calcul de l'estimateur
 
+    estim = exp(-2 * this->mod_->r_ * this->opt_->T_) * ((sumSquare/this->nbSamples_) - pow(sum/this->nbSamples_,2));
+
     //Calcul de l'intervalle de confiance a 95%
+
+    ic = 2 * 1.96 * sqrt(estim) / sqrt(this->nbSamples_);
 }
 
