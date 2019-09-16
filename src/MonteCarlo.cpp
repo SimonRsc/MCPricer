496c4719d40fc89c69a428527740479dcab73780
Cecile <ce.rob.gat@gmail.com>
2019-09-16 11:21:51 +0200

    ajout de asset pour t sup 0

--- src/MonteCarlo.cpp
+++ src/MonteCarlo.cpp
@@ -39,3 +39,37 @@ void MonteCarlo::price(double &prix, double &ic){
     ic = 2 * 1.96 * sqrt(estim) / sqrt(this->nbSamples_);
 }
 
+void MonteCarlo::price(const PnlMat *past, double t, double &prix, double &ic){
+    double sum = 0;
+    double sumSquare = 0;
+    double phi = 0;
+    double estim = 0;
+
+    int nbAssets = this->mod_->size_;
+    int nbTimeSteps = this->opt_->nbTimeSteps_;
+
+    PnlMat *path = pnl_mat_create(nbTimeSteps+1, nbAssets);
+
+    for(int j = 0; j < this->nbSamples_; j++){
+
+        mod_->asset(path, t, this->opt_->T_, nbTimeSteps, this->rng_, past);
+        phi = this->opt_->payoff(path);
+
+        sum += phi;
+        sumSquare += pow(phi, 2);
+
+        }
+
+    pnl_mat_free(& path);
+
+    prix = exp(-this->mod_->r_ * (this->opt_->T_ - t)) * sum / this->nbSamples_;
+
+    //Calcul de l'estimateur
+    estim = (double) exp(-2 * this->mod_->r_ * (this->opt_->T_ - t)) * ((sumSquare/this->nbSamples_) - pow(sum/this->nbSamples_,2));
+
+    //Calcul de l'intervalle de confiance a 95%
+    ic = 2 * 1.96 * sqrt(estim) / sqrt(this->nbSamples_);
+
+    }
+
+
