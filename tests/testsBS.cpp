9c66bc83fbb185e793e990e572d568cc8af739d1
lamurt <tristan.lamur@gmail.com>
2019-09-18 18:31:05 +0200

    Simul Market

--- tests/testsBS.cpp
+++ tests/testsBS.cpp
@@ -7,9 +7,14 @@ using namespace std;
 class BSTest : public ::testing::Test {
 
 protected:
+    PnlRng *rng_;
+
+    BSTest(){
+        rng_ = pnl_rng_create(PNL_RNG_KNUTH);
+    }
+
     virtual void SetUp() {
-        // Code here will be called immediately after the constructor (right
-        // before each test).
+        pnl_rng_sseed(rng_, 1);
     }
 
     virtual void TearDown() {
@@ -34,10 +39,8 @@ TEST_F(BSTest, test_bsNext) {
     PnlVect *vol = pnl_vect_create_from_scalar(2, 0.2);
     PnlVect *spot = pnl_vect_create_from_scalar(2,10);
     auto *BS = new BlackScholesModel(2, 0.02, 0, vol, spot);
-    PnlRng *rng = pnl_rng_create(PNL_RNG_KNUTH);
-    pnl_rng_sseed(rng, 0);
+    pnl_vect_rng_normal(BS->G_, BS->size_, rng_);
     EXPECT_NEAR(0.934, BS->next(1, 0, 1, 0.02), 0.01);
-    pnl_rng_free(&rng);
     delete BS;
 }
 
@@ -45,14 +48,11 @@ TEST_F(BSTest, test_bs1Dim) {
     PnlVect *vol = pnl_vect_create_from_scalar(1, 0.2);
     PnlVect *spot = pnl_vect_create_from_scalar(1,10);
     auto *BS = new BlackScholesModel(1, 0.02, 0, vol, spot);
-    PnlRng *rng = pnl_rng_create(PNL_RNG_KNUTH);
-    pnl_rng_sseed(rng, 1);
     PnlMat *path = pnl_mat_create(11, 1);
-    BS->asset(path, 10, 10, rng);
+    BS->asset(path, 10, 10, rng_);
     for (int i = 0; i < 11; ++i) {
         cout << MGET(path, i, 0) << "\n";
     }
-    pnl_rng_free(&rng);
     pnl_mat_free(&path);
     delete BS;
 }
@@ -61,13 +61,10 @@ TEST_F(BSTest, test_bs5Dim) {
     PnlVect *vol = pnl_vect_create_from_scalar(5, 0.2);
     PnlVect *spot = pnl_vect_create_from_scalar(5,10);
     auto *BS = new BlackScholesModel(5, 0.02, 0.5, vol, spot);
-    PnlRng *rng = pnl_rng_create(PNL_RNG_KNUTH);
-    pnl_rng_sseed(rng, 1);
     PnlMat *path = pnl_mat_create(11, 5);
-    BS->asset(path, 10, 10, rng);
+    BS->asset(path, 10, 10, rng_);
     cout << "\n";
     pnl_mat_print(path);
-    pnl_rng_free(&rng);
     pnl_mat_free(&path);
     delete BS;
 }
@@ -76,17 +73,13 @@ TEST_F(BSTest, test_bsAsset2_1Dim) {
     PnlVect *vol = pnl_vect_create_from_scalar(1, 0.2);
     PnlVect *spot = pnl_vect_create_from_scalar(1,10);
     auto *BS = new BlackScholesModel(1, 0.02, 0, vol, spot);
-    PnlRng *rng = pnl_rng_create(PNL_RNG_KNUTH);
-    pnl_rng_sseed(rng, 1);
     PnlMat *path = pnl_mat_create(21, 1);
     PnlMat *past = pnl_mat_create_from_scalar(10, 1, 10);
 
-    BS->asset(path, 10, 20, 20, rng, past);
+    BS->asset(path, 10, 20, 20, rng_, past);
 
     std::cout << "---------------------" << "\n";
     pnl_mat_print(path);
-
-    pnl_rng_free(&rng);
     pnl_mat_free(&path);
     delete BS;
 }
@@ -106,6 +99,25 @@ TEST_F(BSTest, test_bsShift) {
 
 
     pnl_mat_free(&path);
+    delete BS;
+}
+
+TEST_F(BSTest, test_bsSimul) {
+    PnlVect *vol = pnl_vect_create_from_scalar(5, 0.2);
+    PnlVect *spot = pnl_vect_create_from_scalar(5,10);
+    auto *BS = new BlackScholesModel(5, 0.02, 0, vol, spot);
+
+    PnlMat *simul = pnl_mat_create(11, 5);
+
+    BS->trend_ = pnl_vect_create_from_scalar(5, 0.01);
+    BS->simul_market(simul, 10, 10, rng_);
+
+    std::cout << "\n" << "-------------" << "\n";
+    pnl_mat_print(simul);
+    std::cout << "---------------" << "\n";
+
+
+    pnl_mat_free(&simul);
     delete BS;
 }
 
