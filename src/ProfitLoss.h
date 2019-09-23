b5851f85d9c8d9d98335c23a67de8b27f4cdec0d
Cecile <ce.rob.gat@gmail.com>
2019-09-23 16:27:39 +0200

    doc ProfitLoss

--- src/ProfitLoss.h
+++ src/ProfitLoss.h
@@ -10,6 +10,12 @@
 
 class ProfitLoss {
 public:
+    /// Fonction de calcul du Profit&Loss
+    /// \param[in] monteCarlo
+    /// \param[in] path Trajectoires de marche
+    /// \param[in] H
+    /// \param[in] T
+    /// \param[out] pL Resultat du calcul
     static void PAndL(MonteCarlo *monteCarlo, PnlMat *path, int H, double T, double &pL);
    // void PastForDelta(MonteCarlo *monteCarlo, PnlMat *past, int H, double T, PnlMat *newPast);
 };
